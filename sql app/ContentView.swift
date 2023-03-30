0abdf69d8fd90c8b9af08463d6ccb349793f15db
Educodev <github@educodev.com>
2023-03-30 22:19:13 +0200

    Initial Commit

---
+++ sql app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sql app
+//
+//  Created by Eduardo Ramiro Herrera Anaya on 30/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sql app/sql_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sql_appApp.swift
+//  sql app
+//
+//  Created by Eduardo Ramiro Herrera Anaya on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct sql_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
